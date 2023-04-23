caed0f042890d2fdb058c1e1779f564c2f0e319f
Luna Fang <flunawonder@gmail.com>
2023-04-23 16:39:40 -0400

    enable map filter

--- patents/client/src/pages/PatentMapPage.js
+++ patents/client/src/pages/PatentMapPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState} from 'react';
 import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
@@ -8,24 +8,53 @@ export default function PatentMapPage (){
   const [mapData, setMapData] = useState([]);
   const [geoJsonData, setGeoJsonData] = useState(null);
 
+  const [filters, setFilters] = useState({
+    ml: false,
+    evo: false,
+    nlp: false,
+    speach: false,
+    vision: false,
+    kr: false,
+    planning: false,
+    hardware: false,
+  });
+
+  // initial loading
   useEffect(() => {
-    fetch(`http://${config.server_host}:${config.server_port}/patent_map_filter?pubFrom=2010&pubTo=2020&ml=0&evo=0&nlp=0&speach=0&vision=0&kr=0&planning=0&hardware=0`)
-      .then(res => res.json())
-      .then(resJson => {
-        setMapData(resJson.results);
-    });
+    fetchPatentData(filters);
   }, []);
+
+  const fetchPatentData = (filters) => {
+    const queryString = Object.keys(filters)
+      .map((key) => `${key}=${filters[key] ? 1 : 0}`)
+      .join("&");
+    fetch(
+      `http://${config.server_host}:${config.server_port}/patent_map_filter?pubFrom=2010&pubTo=2020&${queryString}`
+    )
+      .then((res) => res.json())
+      .then((resJson) => {
+        setMapData(resJson.results);
+      });
+  };
 
   useEffect(() => {
     fetch(`${process.env.PUBLIC_URL}/gz_2010_us_040_00_500k.json`)
       .then((res) => res.json())
       .then((data) => {setGeoJsonData(data);
-        console.log('geo data:', data.features);
       });
   }, []);
 
+  const handleChange = (e) => {
+    setFilters({ ...filters, [e.target.name]: e.target.checked });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetchPatentData(filters);
+  };
+
   const onEachFeature = (feature, layer) => {
-   const stateAbbr = getStateAbbreviation(feature.properties.NAME);
+   const stateAbbr = getStateAbbreviation(feature.properties.NAME); //matching abbree and state names
    const stateData = mapData.find((data) => data.state === stateAbbr);
    if (stateData) {
       layer.bindPopup(`<strong>${feature.properties.NAME}</strong><br/>Patents: ${stateData.count}`);
@@ -35,16 +64,91 @@ export default function PatentMapPage (){
   };
 
   const mapStyle = {
-    fillColor: 'white',
-    weight: 2,
-    opacity: 1,
-    color: 'blue',
-    fillOpacity: 0.7
+    fillColor: 'purple',
+    weight: 3,
+    opacity: 0.7,
+    color: 'pastel blue',
+    fillOpacity: 0.1
   };
 
   return (
     <div>
-      <h1>Patent Map</h1>
+      <h3>Patent Map</h3>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input
+            type="checkbox"
+            name="ml"
+            checked={filters.ml}
+            onChange={handleChange}
+          />
+          Machine Learning
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            name="evo"
+            checked={filters.evo}
+            onChange={handleChange}
+          />
+          Evolutionary
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            name="speach"
+            checked={filters.speach}
+            onChange={handleChange}
+          />
+          Speech
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            name="nlp"
+            checked={filters.nlp}
+            onChange={handleChange}
+          />
+          NLP
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            name="vision"
+            checked={filters.vision}
+            onChange={handleChange}
+          />
+          Vision
+        </label>
+        <label>
+        <input
+            type="checkbox"
+            name="kr"
+            checked={filters.kr}
+            onChange={handleChange}
+          />
+          Knowledge
+        </label>
+        <label>
+        <input
+            type="checkbox"
+            name="hardware"
+            checked={filters.hardware}
+            onChange={handleChange}
+          />
+          Hardware
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            name="planning"
+            checked={filters.planning}
+            onChange={handleChange}
+          />
+          Planning
+        </label>
+        <button type="submit">Apply Filters</button>
+      </form>
       {geoJsonData && mapData.length > 0 ? (
       <MapContainer center={[37.8, -96]} zoom={4} style={{ height: '600px', width: '100%' }}>
         <TileLayer
@@ -52,6 +156,7 @@ export default function PatentMapPage (){
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
         <GeoJSON
+          key={mapData.length}
           data={geoJsonData}
           style={mapStyle}
           onEachFeature={onEachFeature}
